4ebafd55316d858c99cff33f769f102ba5ec1aa3
Denys Pavlyuk <densf2@gmail.com>
2022-01-26 19:59:53 +0200

    added_scenarios

---
+++ cypress/integration/pages/auto.ria/general.elements.js
@@ -0,0 +1,8 @@
+
+class GeneralElements {
+    linkInHeader() {
+        return cy.get('.nav-list a.elem-tab');
+    }
+}
+
+export default GeneralElements;
---
+++ cypress/text/text.js
@@ -0,0 +1,4 @@
+export const text_data = {
+    "text_on_main_page": "У чесних продавців і камєнти норм",
+    "text_sell_button": "Продати авто"
+}
