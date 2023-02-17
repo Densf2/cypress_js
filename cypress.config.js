40c20869713ed37a2ee93a9d001098fc226321a9
Denys Pavlyuk <densf2@gmail.com>
2023-02-17 11:34:59 +0200

    moved_to_new_config_file

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
-{
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
     "viewportWidth": 1000,
     "viewportHeight": 890,
     "video": false,
     "reporter": "mochawesome",
+    "supportFile": false,
     "reporterOptions": {
       "reportDir": "report/mochawesome-report",
       "owervrite": false,
       "html": false,
       "json": true 
     }
-  }
-  
+}
+})
