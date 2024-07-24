71097bc5302da6fed38f60cc5c4aa3401f3fc4d1
Denys Pavlyuk <densf2@gmail.com>
2024-07-24 23:44:24 +0300

    changed-resolution

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,19 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    e2e: {
-    "experimentalWebKitSupport": true,
-    "viewportWidth": 1000,
-    "viewportHeight": 890,
-    "video": false,
-    "reporter": "mochawesome",
-    "supportFile": false,
-    "reporterOptions": {
-      "reportDir": "report/mochawesome-report",
-      "owervrite": false,
-      "html": false,
-      "json": true 
-    }
-}
-})
+  e2e: {
+    experimentalWebKitSupport: true,
+    chromeWebSecurity: false,
+    viewportWidth: 1300,
+    viewportHeight: 890,
+    video: false,
+    reporter: "mochawesome",
+    supportFile: false,
+    reporterOptions: {
+      reportDir: "report/mochawesome-report",
+      owervrite: false,
+      html: false,
+      json: true,
+    },
+  },
+});
