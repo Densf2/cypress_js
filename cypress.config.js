3c824be6848ce083d22e4b7eb97e41e1e78ab062
Denys Pavlyuk <densf2@gmail.com>
2024-09-01 15:19:28 +0300

    added-command-for-custom-log-message

--- cypress.config.js
+++ cypress.config.js
@@ -16,12 +16,13 @@ module.exports = defineConfig({
       json: true,
     },
     setupNodeEvents(on, config) {
-      on("task ", {
+      on("task", {
         log(message) {
           console.log(message);
           return null;
         },
       });
+      return config;
     },
   },
 });
