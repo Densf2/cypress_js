c30b667d31d1d942b3fb8189a01e5ec789df516c
Denys Pavlyuk <densf2@gmail.com>
2024-11-22 16:11:17 +0200

    fixed-typo

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,9 +1,5 @@
 /// <reference types="cypress" />
-const {
-  addMatchImageSnapshotCommand,
-} = require("@simonsmith/cypress-image-snapshot/command");
 
-addMatchImageSnapshotCommand();
 Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
