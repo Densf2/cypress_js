860a127be08bc132fd0f2bfbdada379813cbb496
Denys Pavlyuk <densf2@gmail.com>
2024-12-01 16:01:29 +0200

    moved-to-another-site

--- cypress/e2e/itnext_news.page.cy.js
+++ cypress/e2e/itnext_news.page.cy.js
@@ -6,33 +6,24 @@ describe("Verification of product page", () => {
   const homepagelocators = new Homepage();
 
   beforeEach(() => {
-    cy.visit("https://ain.ua");
-    cy.fixture("ain_text").as("aintext");
+    cy.visit("https://itnext.io");
+    cy.fixture("itnext_text").as("itntext");
   });
 
   it("Check main page text", () => {
-    cy.get("@aintext").then((ainText) => {
-      cy.get("h1.strong").should("contain", ainText.golovna_text);
+    cy.get("@itntext").then((itnText) => {
+      cy.title().should("contain", itnText.page_title);
     });
   });
 
-  it("Check side news block", () => {
+  it.skip("Check side news block", () => {
     cy.get("@aintext").then((ainText) => {
-      cy.get("a.widget__content img").eq(0).should("be.visible");
-      cy.get("ul.main-nav__menu li.current-page")
-        .find("a")
-        .should("contain", ainText.golovna_text);
-      cy.get("ul.main-nav__menu li a")
-        .filter(":contains('Технології')")
-        .should("be.visible");
-      cy.get("ul.main-nav__menu li a")
-        .filter(":contains('Огляди')")
-        .should("be.visible");
+      cy.get("nav div.buttonSet li").eq(0).should("contain", "About ITNEXT");
     });
   });
 
   // the test below without uploadig the text from fixtures
-  it("Check the list of bussiness news", () => {
+  it.skip("Check the list of bussiness news", () => {
     cy.get("ul.main-nav__menu li a[href='/business/']").click();
     cy.url().should("include", "/business");
     cy.get("ul.widget__header_tags li a").eq(0).should("contain", "Бізнес");
@@ -40,7 +31,7 @@ describe("Verification of product page", () => {
     cy.get("a.widget__content p.h2").should("have.length.greaterThan", 5);
   });
 
-  it("Check the review news", () => {
+  it.skip("Check the review news", () => {
     cy.get("ul.main-nav__menu li a[href='/reviews/']").click();
     cy.url().should("include", "/reviews");
     cy.get("ul.widget__header_tags li a").eq(0).should("contain", "Огляди");
