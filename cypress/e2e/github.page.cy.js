29d999075c4accbcc92fb1e38f908436eec9b84c
Denys Pavlyuk <densf2@gmail.com>
2024-12-01 17:32:54 +0200

    in-progress-changes

--- cypress/e2e/github.page.cy.js
+++ cypress/e2e/github.page.cy.js
@@ -3,13 +3,17 @@ import Homepage from "./pages/github/elements";
 describe("GitHub", () => {
   const homepage = new Homepage();
 
-  context("Homepage", () => {
+  context("Security Page", () => {
     beforeEach(() => {
-      cy.visit("https://github.com");
+      cy.visit("https://github.com/features/security");
     });
 
     it("check logo", () => {
       homepage.logoHeader().should("be.visible");
+      cy.wait(1000);
+      cy.get(
+        "div[data-testid='contentful-bg-image-container']"
+      ).matchImageSnapshot();
     });
   });
 });
