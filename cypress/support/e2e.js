1dd1b0ee8e0de06ab715223b90c60045853492a6
Denys Pavlyuk <densf2@gmail.com>
2024-12-29 10:02:42 +0200

    removed-unused-param

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,10 +4,6 @@ import { addMatchImageSnapshotCommand } from "@simonsmith/cypress-image-snapshot
 
 addMatchImageSnapshotCommand();
 
-addMatchImageSnapshotCommand({
-  failureThreshold: 0.2,
-});
-
 Cypress.on("uncaught:exception", (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
