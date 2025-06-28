184cb02981d6eb167b0f7aace65ec8254a0f5373
Denys Pavlyuk <densf2@gmail.com>
2025-06-28 22:10:28 +0300

    adde-package-for-cypress

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import pluginCypress from "eslint-plugin-cypress";
+
+export default defineConfig([
+  // { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  // { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  pluginCypress.configs.recommended,
+  { files: ["cypress/**/*.js"] },
+]);
