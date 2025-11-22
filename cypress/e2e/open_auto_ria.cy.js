6e0eb31e5b9487ff4ab821b2c40f805c467c1ae7
Denys Pavlyuk <densf2@gmail.com>
2025-11-22 15:02:56 +0200

    moved-to-use-another-site

--- cypress/e2e/open_auto_ria.cy.js
+++ cypress/e2e/open_auto_ria.cy.js
@@ -1,60 +1,171 @@
-import { text_data } from "../text/text";
-import GeneralElements from "./pages/auto.ria/general.elements";
+/// <reference types="Cypress" />
 
-describe("Open main page and verify functionality", () => {
-  const generalElements = new GeneralElements();
+describe("JPetStore E-Commerce Application", () => {
+  beforeEach(() => {
+    cy.visit("https://jpetstore.aspectran.com/");
+  });
 
-  context("verification of FE", () => {
-    beforeEach(() => {
-      cy.visit("https://auto.ria.com/uk/");
-    });
-    it("check text in header", () => {
-      cy.get(".container-header a.button-add").should(
-        "contain",
-        text_data.text_sell_button
-      );
-      generalElements.linkInHeader().eq(0).should("contain", "Вживані авто");
-      generalElements.linkInHeader().eq(1).should("contain", "Нові авто");
-      generalElements.linkInHeader().eq(2).should("contain", "Новини");
-      generalElements.linkInHeader().eq(3).should("contain", "Все для авто");
-      cy.get("h2.showcase-head").should("be.visible");
-      cy.get("nav.transport-type > a.item-tt.active > span").should(
-        "contain",
-        "Легкові б/у"
-      );
+  context("Homepage Tests", () => {
+    it("should load the homepage successfully", () => {
+      cy.url().should("eq", "https://jpetstore.aspectran.com/");
+      cy.contains("JPetStore Demo").should("be.visible");
+      cy.contains("The goal of the JPetStore Demo App").should("be.visible");
     });
 
-    it("check search", () => {
-      cy.get("div.form-search").should("be.visible");
-      cy.get('label[for="forYear"]').eq(0).click();
-      cy.get("select#yearFrom").select("2015");
-      cy.get("select#yearTo").select("2020");
-      cy.get("div.footer-form button.full").click({ force: true });
-      cy.url().should(
-        "include",
-        "search/?categories.main.id=1&indexName=auto,order_auto,newauto_search&year[0].gte=2015&year[0].lte=2020&size=20"
-      );
-      cy.get("section.ticket-item").should("have.length", 21);
+    it("should display all product categories", () => {
+      cy.contains("a", "Fish").should("be.visible");
+      cy.contains("a", "Dogs").should("be.visible");
+      cy.contains("a", "Cats").should("be.visible");
+      cy.contains("a", "Reptiles").should("be.visible");
+      cy.contains("a", "Birds").should("be.visible");
+    });
+
+    it("should display navigation links", () => {
+      cy.contains("a", "Sign In").should("be.visible");
+      cy.contains("a", "Sign Up").should("be.visible");
+      cy.get('a[href*="cart/viewCart"]').should("be.visible");
+      cy.get('a[href*="help.html"]').should("be.visible");
     });
   });
 
-  context("check functionality", () => {
-    beforeEach(() => {
-      cy.visit("https://auto.ria.com/uk/");
-    });
-    it("check redirect to details page", () => {
-      cy.get("div.mhide a").eq(3).click();
-      cy.get("h1.auto-head_title").should("contain", /\w+/g);
-      cy.task("log", "logs:redirect to details page is correct");
-      generalElements.siteLogoInHeader().should("have.css", "opacity", "0");
-      cy.task("log", "logs:checked logo in header");
+  context("Category Navigation Tests", () => {
+    it("should navigate to Fish category", () => {
+      cy.contains("a", "Fish").click();
+      cy.url().should("include", "/categories/FISH");
+      cy.contains("h3", "Fish").should("be.visible");
+      cy.contains("Angelfish").should("be.visible");
+      cy.contains("Tiger Shark").should("be.visible");
+      cy.contains("Koi").should("be.visible");
+      cy.contains("Goldfish").should("be.visible");
     });
 
-    it("check news page", () => {
-      cy.get('a[data-type="news"]', { timeout: 3000 }).click();
-      cy.url().should("include", "/news/");
-      generalElements.siteLogoInHeader().should("have.css", "opacity", "0");
-      cy.get("input#fieldTextSearch").should("be.visible");
+    it("should navigate to Dogs category", () => {
+      cy.contains("a", "Dogs").click();
+      cy.url().should("include", "/categories/DOGS");
+      cy.contains("h3", "Dogs").should("be.visible");
+    });
+
+    it("should navigate to Cats category", () => {
+      cy.contains("a", "Cats").click();
+      cy.url().should("include", "/categories/CATS");
+      cy.contains("h3", "Cats").should("be.visible");
+    });
+
+    it("should navigate to Reptiles category", () => {
+      cy.contains("a", "Reptiles").click();
+      cy.url().should("include", "/categories/REPTILES");
+      cy.contains("h3", "Reptiles").should("be.visible");
+    });
+
+    it("should navigate to Birds category", () => {
+      cy.contains("a", "Birds").click();
+      cy.url().should("include", "/categories/BIRDS");
+      cy.contains("h3", "Birds").should("be.visible");
+    });
+
+    it("should return to main menu from category page", () => {
+      cy.contains("a", "Fish").click();
+      cy.contains("a", "Return to Main Menu").click();
+      cy.url().should("eq", "https://jpetstore.aspectran.com/");
+    });
+  });
+
+  context("Product Listing Tests", () => {
+    beforeEach(() => {
+      cy.visit("https://jpetstore.aspectran.com/categories/FISH");
+    });
+
+    it("should display product list in category", () => {
+      cy.get("table").should("be.visible");
+      cy.contains("td", "FI-SW-01").should("be.visible");
+      cy.contains("td", "Angelfish").should("be.visible");
+    });
+
+    it("should click on a product to view details", () => {
+      cy.contains("a", "FI-SW-01").click();
+      cy.url().should("include", "/products/FI-SW-01");
+    });
+  });
+
+  context("Authentication Tests", () => {
+    it("should navigate to Sign In page", () => {
+      cy.contains("a", "Sign In").click();
+      cy.url().should("include", "/account/signonForm");
+      cy.contains("Please enter your username and password").should(
+        "be.visible"
+      );
+      cy.get('input[name="username"]').should("be.visible");
+      cy.get('input[name="password"]').should("be.visible");
+    });
+
+    it("should navigate to Sign Up page", () => {
+      cy.contains("a", "Sign Up").click();
+      cy.url().should("include", "/account/newAccountForm");
+    });
+
+    it("should display Sign In form elements", () => {
+      cy.visit("https://jpetstore.aspectran.com/account/signonForm");
+      cy.get('input[name="username"]').should("be.visible");
+      cy.get('input[name="password"]').should("be.visible");
+      cy.contains("button", "Login").should("be.visible");
+      cy.contains("a", "Register Now!").should("be.visible");
+    });
+
+    it("should attempt login with demo credentials", () => {
+      cy.visit("https://jpetstore.aspectran.com/account/signonForm");
+      cy.get('input[name="username"]').type("j2ee");
+      cy.get('input[name="password"]').type("j2ee");
+      cy.contains("button", "Login").click();
+      // Verify successful login or redirect
+      cy.url().should("not.include", "/account/signonForm");
+    });
+  });
+
+  context("Shopping Cart Tests", () => {
+    it("should navigate to cart page", () => {
+      cy.get('a[href*="cart/viewCart"]').click();
+      cy.url().should("include", "/cart/viewCart");
+    });
+
+    it("should display empty cart message when no items", () => {
+      cy.visit("https://jpetstore.aspectran.com/cart/viewCart");
+      // Cart page should load (may show empty cart message)
+      cy.url().should("include", "/cart/viewCart");
+    });
+  });
+
+  context("Help and Information Tests", () => {
+    it("should open help page", () => {
+      cy.get('a[href*="help.html"]').click();
+      // Help page should open
+    });
+  });
+
+  context("Footer and External Links Tests", () => {
+    it("should display footer links", () => {
+      cy.contains("a", "aspectran.com").should("be.visible");
+      cy.contains("a", "mybatis.org").should("be.visible");
+    });
+
+    it("should display Aspectran information", () => {
+      cy.contains("ABOUT ASPECTRAN").should("be.visible");
+      cy.contains(
+        "Aspectran is a lightweight, high‑performance framework"
+      ).should("be.visible");
+    });
+  });
+
+  context("Responsive Design Tests", () => {
+    it("should display correctly on mobile viewport", () => {
+      cy.viewport(375, 667);
+      cy.contains("JPetStore Demo").should("be.visible");
+      cy.contains("a", "Fish").should("be.visible");
+    });
+
+    it("should display correctly on tablet viewport", () => {
+      cy.viewport(768, 1024);
+      cy.contains("JPetStore Demo").should("be.visible");
+      cy.contains("a", "Fish").should("be.visible");
     });
   });
 });
