c4b6c792555a5b594ebc514d0dc625f3cbaa090e
Denys Pavlyuk <densf2@gmail.com>
2026-01-28 11:39:28 +0200

    updated-code-for-open-ui-playground

--- cypress/e2e/open.eshop.cy.js
+++ cypress/e2e/open.eshop.cy.js
@@ -1,38 +1,136 @@
 /// <reference types="cypress" />
 
-import Homepage from "./pages/homepage";
-
-describe.skip("testing eshop", () => {
-  const homepagelocators = new Homepage();
+describe("UI Test Automation Playground", () => {
   beforeEach(() => {
-    cy.visit("https://prego.ua/uk/new-products");
-    cy.fixture("eshop_text.json").as("text");
+    cy.visit("http://uitestingplayground.com/");
   });
 
-  it("open product page", () => {
-    // example of loading the selector from the file
-    homepagelocators.searchInput().click().type("черевики");
-    cy.get("#search-submit").click();
-    cy.url().should(
-      "include",
-      "https://prego.ua/uk/internal/full-search-result?query"
+  it("should load the homepage successfully", () => {
+    cy.url().should("include", "uitestingplayground.com");
+    cy.title().should("contain", "UI Test Automation Playground");
+  });
+
+  it("should display navigation bar elements", () => {
+    cy.get("nav.navbar").should("be.visible");
+    cy.get("a.navbar-brand").should("be.visible").and("contain", "UITAP");
+    cy.get('a.nav-link[href="/home"]')
+      .should("be.visible")
+      .and("contain", "Home");
+    cy.get('a.nav-link[href="/resources"]')
+      .should("be.visible")
+      .and("contain", "Resources");
+  });
+
+  it("should display main title and description", () => {
+    cy.get("h1#title")
+      .should("be.visible")
+      .and("contain", "UI Test Automation");
+    cy.get("blockquote#citation").should("be.visible");
+    cy.get("blockquote#citation p.mb-0").should(
+      "contain",
+      "Quality is not an act, it is a habit"
     );
+    cy.get("blockquote#citation footer").should("contain", "Aristotle");
   });
 
-  it("search by specific request", () => {
-    cy.get("@text").then((text) => {
-      cy.get("input#searchFormQueryInput").click();
-      cy.get("input#searchFormQueryInput").type(text.search_text_sneaker);
-    });
+  it("should display alert warning message", () => {
+    cy.get("div.alert-warning")
+      .should("be.visible")
+      .and(
+        "contain",
+        "The purpose of this website is to provide a platform for sharpening UI test automation skills"
+      );
+  });
 
-    it("clicking on the delivery link", () => {
-      cy.get(
-        "div.clearfix.header-wrap > ul.nav-top.header__left > li:nth-child(1) > a"
-      ).click();
-      cy.url().should("contain", "/uk/dostavka");
-      cy.get("h2.static-page__header")
-        .should("be.visible")
-        .contains("Доставка");
-    });
+  it("should display Dynamic ID section link", () => {
+    cy.get('a[href="/dynamicid"]')
+      .should("be.visible")
+      .and("contain", "Dynamic ID");
+    cy.get('a[href="/dynamicid"]')
+      .parent()
+      .next("p")
+      .should("contain", "Make sure you are not recording dynamic IDs");
+  });
+
+  it("should display Class Attribute section link", () => {
+    cy.get('a[href="/classattr"]')
+      .should("be.visible")
+      .and("contain", "Class Attribute");
+  });
+
+  it("should display Hidden Layers section link", () => {
+    cy.get('a[href="/hiddenlayers"]')
+      .should("be.visible")
+      .and("contain", "Hidden Layers");
+  });
+
+  it("should display Load Delay section link", () => {
+    cy.get('a[href="/loaddelay"]')
+      .should("be.visible")
+      .and("contain", "Load Delay");
+  });
+
+  it("should display AJAX Data section link", () => {
+    cy.get('a[href="/ajax"]').should("be.visible").and("contain", "AJAX Data");
+  });
+
+  it("should display Click section link", () => {
+    cy.get('a[href="/click"]').should("be.visible").and("contain", "Click");
+  });
+
+  it("should display Text Input section link", () => {
+    cy.get('a[href="/textinput"]')
+      .should("be.visible")
+      .and("contain", "Text Input");
+  });
+
+  it("should display Visibility section link", () => {
+    cy.get('a[href="/visibility"]')
+      .should("be.visible")
+      .and("contain", "Visibility");
+  });
+
+  it("should display Sample App section link", () => {
+    cy.get('a[href="/sampleapp"]')
+      .should("be.visible")
+      .and("contain", "Sample App");
+  });
+
+  it("should display Progress Bar section link", () => {
+    cy.get('a[href="/progressbar"]')
+      .should("be.visible")
+      .and("contain", "Progress Bar");
+  });
+
+  it("should display footer with copyright info", () => {
+    cy.get("footer#footer").should("be.visible");
+    cy.get("footer#footer").should("contain", "Fork the website on GitHub");
+    cy.get("footer#footer").should("contain", "Inflectra Corporation");
+  });
+
+  it("should navigate to Home page", () => {
+    cy.get('a.nav-link[href="/home"]').click();
+    cy.url().should("include", "/home");
+  });
+
+  it("should navigate to Resources page", () => {
+    cy.get('a.nav-link[href="/resources"]').click();
+    cy.url().should("include", "/resources");
+  });
+
+  it("should navigate to Dynamic ID page", () => {
+    cy.get('a[href="/dynamicid"]').click();
+    cy.url().should("include", "/dynamicid");
+  });
+
+  it("should navigate to Sample App page", () => {
+    cy.get('a[href="/sampleapp"]').click();
+    cy.url().should("include", "/sampleapp");
+  });
+
+  it("should display cube image", () => {
+    cy.get("img.img-fluid")
+      .should("be.visible")
+      .and("have.attr", "alt", "Responsive image");
   });
 });
